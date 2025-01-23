a29bcc1ac79b67adf610159281df4a3635d75300
Stephen Fuchs <stephen@stephenfuchs.net>
2025-01-23 13:29:37 -0600

    add dontation total trend count

--- components/ItemsDonations.vue
+++ components/ItemsDonations.vue
@@ -15,13 +15,61 @@ const { salesList } = useItemSales(orders, previousOrders, exclude);
 const donations = computed(() => {
     return salesList.value.filter((item) => item.category === "donations");
 });
+
+const totalDonations = computed(() => {
+    return donations.value.reduce((total, donation) => {
+        return total + donation.grossSales;
+    }, 0);
+});
+
+const trendDonations = computed(() => {
+    return donations.value.reduce((total, donation) => {
+        return total + donation.trendGrossSales;
+    }, 0);
+});
 </script>
 
 <template>
     <UiAppCard>
         <template #title> Donations </template>
         <template #options>
-            <Tag severity="success" value="$19.00" />
+            <div class="flex items-center gap-4">
+                <Tag
+                    :severity="
+                        trendDonations > 0
+                            ? 'success'
+                            : trendDonations < 0
+                              ? 'danger'
+                              : 'secondary'
+                    "
+                    :value="formatCurrency(totalDonations)"
+                />
+                <div
+                    class="flex w-1/2 gap-1 text-sm font-semibold"
+                    :class="{
+                        'text-green-500': trendDonations >= 0,
+                        'text-orange-500': trendDonations < 0,
+                        'text-muted-color': trendDonations === 0,
+                    }"
+                >
+                    <div class="material-symbols-outlined flex-1 text-end">
+                        {{
+                            trendDonations > 0
+                                ? "trending_up"
+                                : trendDonations < 0
+                                  ? "trending_down"
+                                  : "trending_flat"
+                        }}
+                    </div>
+                    <div class="flex-1">
+                        {{
+                            trendDonations < 0
+                                ? formatCurrency(Math.abs(trendDonations))
+                                : formatCurrency(trendDonations)
+                        }}
+                    </div>
+                </div>
+            </div>
         </template>
         <div class="flex flex-col gap-4">
             <div v-if="donations?.length === 0" class="mt-4 self-center italic">
