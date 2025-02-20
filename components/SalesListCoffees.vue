ec26f8ae926697e408878a824b77fd3fd6b07c4d
Stephen Fuchs <stephen@stephenfuchs.net>
2025-02-20 14:21:46 -0600

    add coffee donation sales data to coffee list

--- components/SalesListCoffees.vue
+++ components/SalesListCoffees.vue
@@ -1,4 +1,5 @@
 <script setup type="ts">
+import { formatCurrency } from "~/server/utils/formatCurrency";
 const filters = useFilters();
 const { orders } = useOrders(filters.startDate, filters.endDate);
 const { orders: previousOrders } = useOrders(
@@ -14,17 +15,44 @@ const { salesList } = useSalesList(
 
 const coffees = computed(() => {
     const coffeeNames = ["regular", "hazelnut", "french vanilla", "caramel", "decaf"]
-    let filteredList = salesList.value.filter((item) => coffeeNames.includes(item.name));
+    let filteredList = salesList.value.filter((item) => coffeeNames.includes(item.name)).map((item) => ({
+        name: item.name,
+        imgCoffee: item.imgCoffee,
+        quantity: item.quantity,
+        trendQuantity: item.trendQuantity
+    }));
 
     return filteredList.sort((a,b) => b.quantity - a.quantity);
 });
+
+const coffeeDonations = computed(() => {
+    const donation = salesList.value.find((item) => item.name === "coffee donation")
+    return donation ? {
+        grossSales: donation.grossSales,
+        trendGrossSales: donation.trendGrossSales
+    } : null
+})
 </script>
 
 <template>
     <UiAppCard full>
         <template #title> Brewed Coffees </template>
         <template #options>
-            <Tag severity="success" value="$19.00" />
+            <div
+                :class="
+                    coffeeDonations?.trendGrossSales > 0
+                        ? 'bg-green-100 text-green-700'
+                        : coffeeDonations?.trendGrossSales < 0
+                          ? 'bg-orange-100 text-orange-700'
+                          : 'bg-neutral-200 text-neutral-700'
+                "
+                class="flex items-center gap-1 self-start rounded px-2 py-1 text-sm font-bold"
+            >
+                <div class="material-symbols-outlined">money_bag</div>
+                <div>
+                    {{ formatCurrency(coffeeDonations?.grossSales || 0) }}
+                </div>
+            </div>
         </template>
         <SalesList :source="coffees" type="coffee" />
     </UiAppCard>
