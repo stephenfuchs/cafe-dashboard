7838702a43e33dde0e95578f112ca2f141f719a5
Stephen Fuchs <stephen@stephenfuchs.net>
2025-07-25 14:19:52 -0500

    add exclusion date for Summer Adventure

--- server/utils/excludes.ts
+++ server/utils/excludes.ts
@@ -1,12 +1,17 @@
-// Exclude Wine to Water events that used Square for processing
+// Exclude non-cafe events (Wine to Water, Summer Adventure, etc) that used Square for processing
 export const excludeDate: Set<string> = new Set([
-    "10-21-2017", "11-10-2018", "11-09-2019", "11-12-2021", "11-13-2021", "11-11-2022", "11-12-2022"
+    "10-21-2017",
+    "11-10-2018",
+    "11-09-2019",
+    "11-12-2021",
+    "11-13-2021",
+    "11-11-2022",
+    "11-12-2022",
+    "07-23-2025",
 ]);
 
 // Exclude items from being processed
-export const excludeItem: Set<string> = new Set([
-    "Silent Auction"
-]);
+export const excludeItem: Set<string> = new Set(["Silent Auction"]);
 
 export const excludeDonations: Array<string> = [
     "coffee pot",
